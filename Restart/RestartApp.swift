3be1e96bcba79156992fe1b705c5e3e7e02808a6
kan1657 <kan1657@hotmail.com>
2022-03-03 00:32:43 +0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Kan Kunnawat on 3/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
